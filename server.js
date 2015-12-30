94cb4c8a2c985c9404aaf3bfdb74d481e2d98384
Jordan Boyer <jordan.boyer@epitech.eu>
2015-12-30 16:51:57 +0100

    add Post request

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: "1",
-	description: "Meet mon for lunch",
-	completed: false
-}, {
-	id: "2",
-	description: "Go to market",
-	completed: false
-}, {
-	id: "3",
-	description: "Go to The Airport",
-	completed: true
-}];
+var todos = [];
+var todoNextID = 1
+
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
 	res.send('Todo API Root');
@@ -24,7 +16,7 @@ app.get("/todos", function (req, res) {
 })
 
 app.get("/todos/:id", function (req, res) {
-	var todoId = req.params.id;
+	var todoId = parseInt(req.params.id, 10);
 
 	for (var i = 0; i < todos.length; i++) {
 		if (todos[i].id === todoId) {
@@ -33,6 +25,13 @@ app.get("/todos/:id", function (req, res) {
 		}
 	}
 	res.status(404).send();
+})
+
+app.post("/todos", function (req, res) {
+	var body = req.body;
+	body.id = todoNextID++;
+	todos.push(body);
+	res.json(body);
 })
 
 app.listen(PORT, function() {
