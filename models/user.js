d4f8f2d37373ecbea098ed29393a7a88f22123fb
Jordan Boyer <jordan.boyer@epitech.eu>
2016-01-02 22:58:28 +0100

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require("bcrypt");
+var _ = require("underscore");
+
 module.exports = function(sequelize, dataTypes) {
 	return sequelize.define("user", {
 		email: {
@@ -8,19 +11,39 @@ module.exports = function(sequelize, dataTypes) {
 				isEmail: true
 			}
 		},
+		salt: {
+			type: dataTypes.STRING
+		},
+		password_hash: {
+			type: dataTypes.STRING
+		},
 		password: {
-			type: dataTypes.STRING,
+			type: dataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [8, 100]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue("password", value);
+				this.setDataValue("salt", salt);
+				this.setDataValue("password_hash", hashedPassword);
 			}
 		}
 	}, {
 		hooks: {
-			beforeValidate: function (user, options) {
+			beforeValidate: function(user, options) {
 				if (typeof user.email === "string") {
 					user.email = user.email.toLowerCase();
 				}
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, "id", "email", "updatedAt", "createdAt");
 			}
 		}
 	});
