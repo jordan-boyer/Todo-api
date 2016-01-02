8bafb2204ef4c2cf2aae182e80cdae61158f3298
Jordan Boyer <jordan.boyer@epitech.eu>
2016-01-02 18:56:59 +0100

    Convert PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -71,12 +71,11 @@ app.delete("/todos/:id", function(req, res) {
 	db.todo.destroy({
 		where: where
 	}).then(function(rowsDeleted) {
-		if (rowsDeleted === 0){
+		if (rowsDeleted === 0) {
 			res.status(404).json({
 				error: "No todo with id"
 			});
-		}
-		else {
+		} else {
 			res.status(204).send();
 		}
 	}, function(e) {
@@ -86,29 +85,30 @@ app.delete("/todos/:id", function(req, res) {
 
 app.put("/todos/:id", function(req, res) {
 	var body = _.pick(req.body, "description", "completed");
-	var validAttributes = {};
+	var attributes = {};
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+
+	if (body.hasOwnProperty("completed")) {
+		attributes.completed = body.completed;
+	}
+
+	if (body.hasOwnProperty("description")) {
+		attributes.description = body.description;
+	}
+
+	db.todo.findById(todoId).then(function(todo) {
+		if (todo) {
+			todo.update(attributes).then(function(todo) {
+				res.json(todo.toJSON());
+			}, function(e) {
+				res.status(400).json(e);
+			});
+		} else {
+			res.status(404).send();
+		}
+	}, function() {
+		res.status(500).send();
 	});
-
-	if (!matchedTodo)
-		return res.status(404).send();
-
-	if (body.hasOwnProperty("completed") && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty("completed")) {
-		return res.status(400).send();
-	}
-
-	if (body.hasOwnProperty("description") && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty("description")) {
-		return res.status(400).send();
-	}
-
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
 });
 
 db.sequelize.sync().then(function() {
