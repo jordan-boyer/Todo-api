5435a62bafe959691a5db64f73bddfb2231fefc5
Jordan Boyer <jordan.boyer@epitech.eu>
2016-01-02 18:28:24 +0100

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ app.get("/todos", function(req, res) {
 		};
 	}
 
-	db.todo.findAll({where: where}).then(function(todos) {
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
 	}, function(e) {
 		res.status(500).send();
@@ -62,16 +64,24 @@ app.post("/todos", function(req, res) {
 
 app.delete("/todos/:id", function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
+	var where = {
 		id: todoId
-	});
+	};
 
-	if (matchedTodo) {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.destroy({
+		where: where
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0){
+			res.status(404).json({
+				error: "No todo with id"
+			});
+		}
+		else {
+			res.status(204).send();
+		}
+	}, function(e) {
+		res.status(500).send();
+	});
 });
 
 app.put("/todos/:id", function(req, res) {
